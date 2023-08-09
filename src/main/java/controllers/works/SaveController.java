71332dbb9a79a4d16acd246387e4ac1c39e610b6
sjroh88 <sjroh88@gmail.com>
2023-08-09 19:58:27 +0900

    day05 작업 목록 정리

--- src/main/java/controllers/works/SaveController.java
+++ src/main/java/controllers/works/SaveController.java
@@ -1,5 +1,8 @@
 package controllers.works;
 
+import static commons.ScriptUtils.*;
+
+import commons.ScriptUtils;
 import commons.ViewUtils;
 import controllers.Controller;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,11 +10,22 @@ import jakarta.servlet.http.HttpServletResponse;
 import models.works.SaveService;
 import models.works.WorkServiceManager;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SaveController implements Controller {
     @Override
     public void get(HttpServletRequest req, HttpServletResponse resp) {
         String URI = req.getRequestURI();
         String mode = URI.indexOf("edit") != -1 ? "edit" : "add";
+        if(mode.equals("edit")){
+            long workNo = getWorkNo(req);
+
+        }
+
+
 
         String[] addScript = {"ckeditor/ckeditor","work/form"};
         req.setAttribute("addScript",addScript);
@@ -24,10 +38,29 @@ public class SaveController implements Controller {
         SaveService saveService = WorkServiceManager.getInstance().saveService();
         try{
             saveService.save(req);
-
+            go(resp,req.getContextPath()+"/work","parent");
         }catch(Exception e){
+            alertError(resp,e);
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 작업 번호 추출
+     *
+     * @param req
+     * @return
+     */
+    private long getWorkNo(HttpServletRequest req){
+        String URI = req.getRequestURI();
+
+        String pattern = "edit/(\\d*)";
+        Pattern p = Pattern.compile(pattern);
+        Matcher matcher = p.matcher(URI);
+        if(matcher.find()){
+            return Long.parseLong(matcher.group(1));
+        }
+        return 0L;
     }
 }
